5088dbb3dc4e76c3edbbd5f4bc4a1111de6881f5
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-07-30 22:55:52 +0600

    hes code added and copy function added

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ function main() {
     const Bg = document.getElementById('root');
     const cta = document.getElementById('change-btn');
     const alpha = document.getElementById('opacity-btn');
-
+    const hexcode = document.getElementById('output');
+    const btnCopy = document.getElementById('copy-btn');
     // bg changer
     cta.addEventListener('click', function () {
         const bgColor = rgbColorGenerator();
         Bg.style.backgroundColor = bgColor;
+        hexcode.textContent = bgColor;
     });
+
+
+   
     // .opacity
     alpha.addEventListener("click", function () {
         var opvalue = Math.random();
         Bg.style.opacity = opvalue;
-    })
+    });
+
+     btnCopy.addEventListener("click", function () {
+        navigator.clipboard.writeText(hexcode.textContent);
+        alert("copied successfully👌👌");
+    });
+
 }
 
 
@@ -24,5 +35,6 @@ function rgbColorGenerator() {
     var red = Math.floor(Math.random() * 255);
     var green = Math.floor(Math.random() * 255);
     var blue = Math.floor(Math.random() * 255);
-    return `rgb(${red}, ${green}, ${blue})`; 
+    // return `rgb(${red.toString(16)}, ${green.toString(16)}, ${blue.toString(16)})`; 
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
